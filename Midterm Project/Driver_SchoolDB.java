1582914f4a6c1f8a684b317195e3a365209daa97
sarahdipietra <47428007+sarahdipietra@users.noreply.github.com>
2020-03-29 23:49:09 -0400

    Update Driver_SchoolDB.java

--- Midterm Project/Driver_SchoolDB.java
+++ Midterm Project/Driver_SchoolDB.java
@@ -5,6 +5,9 @@ import java.io.IOException;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import static java.lang.Integer.parseInt;
 
 public class Driver_SchoolDB {
 	public static void main(String[] args) throws IOException {
@@ -22,110 +25,191 @@ public class Driver_SchoolDB {
 		Student[] students = new Student[50];
 		int numOfStudents = 0;
 		
-		try {
-			scnr = new Scanner("SchoolDB_Initial.txt");
-			while (scnr.hasNextLine()) {
-				String line = scnr.nextLine();
-				String getRidOfCommas = line.replace(", ", "");
-				String[] array = line.split(" ");
+			scnr = new Scanner("SchoolDB_Initial.txtf");
 
-				if (array[0] == "Course:") {
-					Course course1 = new Course(Boolean.parseBoolean(array[1]), Integer.parseInt(array[2]), array[3], Integer.parseInt(array[4]));
-					courses[numOfCourses] = course1;
-					numOfCourses++;
-					System.out.println(numOfCourses);
-				}
-				
-				else if (array[0] == "Faculty:") {
-					if (array.length == 1) {
-						Faculty teacher = new Faculty();
-						faculty[numOfFaculty] = teacher;
-						numOfFaculty++;
-					}
-					if (array.length == 2) {
-						Faculty teacher = new Faculty(Boolean.parseBoolean(array[1]));
-						faculty[numOfFaculty] = teacher;
-						numOfFaculty++;
-					}
-					if (array.length == 3) {
-						Faculty teacher = new Faculty(array[1], Boolean.parseBoolean(array[2]));
-						faculty[numOfFaculty] = teacher;
-						numOfFaculty++;
-					}
-					if (array.length == 5) {
-						Faculty teacher = new Faculty(array[1], Integer.parseInt(array[2]), array[3], Boolean.parseBoolean(array[4]));
-						faculty[numOfFaculty] = teacher;
-						numOfFaculty++;
-					}
-					else {
+	        while (scnr.hasNextLine()) {
+	            String personType = scnr.next();
+	            if (personType.equals("Course:")) {
+	                String[] teacherString = scnr.nextLine().split(",");
+	                
+	                if (teacherString[0].equals(" true") || teacherString[0].equals(" false")) {
+	                    Boolean personIsGraduate = false;
+	                    
+	                    if (teacherString[0].equals(" true")) {
+	                        personIsGraduate = true;
+	                    }
+	                    else if (teacherString.length > 1) {
+	                        int personCourseNum = parseInt(teacherString[1].replace(" ", ""));
+	                        String personCourseDept = teacherString[2].replace(" ", "");
+	                        int personNumCredits = parseInt(teacherString[3].replace(" ", ""));
+	                        Course course = new Course(personIsGraduate, personCourseNum, personCourseDept, personNumCredits);
+	                        courses[numOfCourses] = course;
+	                        numOfCourses++;
+	                    }
+	                    else {
+	                        Course course = new Course();
+	                        courses[numOfCourses] = course;
+	                        numOfCourses++;
+	                    }
+	                }
+	            }
+	            
+	            else if (personType.equals("GeneralStaff:")) {
+	                String[] GenStaffString = scnr.nextLine().split(",");
+	                if (GenStaffString.length == 0) {
+	                    GeneralStaff staffMember1 = new GeneralStaff();
+	                    generalStaff[numOfGeneralStaff] = staffMember1;
+	                    numOfGeneralStaff++;
+	                }
+	                else if (GenStaffString.length == 1) {
+	                	GenStaffString[0] = GenStaffString[0].trim();
+	                    GeneralStaff staffMember1 = new GeneralStaff(GenStaffString[0]);
+	                    generalStaff[numOfGeneralStaff] = staffMember1;
+	                    numOfGeneralStaff++;
+	                }
+	                else if (GenStaffString.length == 2) {
+	                    GeneralStaff staffMember1 = new GeneralStaff(GenStaffString[0], GenStaffString[1]);
+	                    generalStaff[numOfGeneralStaff] = staffMember1;
+	                    numOfGeneralStaff++;
+	                }
+	                else if (GenStaffString.length == 4) {
+	                	GeneralStaff staffMember1 = new GeneralStaff(GenStaffString[0], Integer.parseInt(GenStaffString[1].replace(" ", "")), GenStaffString[2], GenStaffString[3]);
+	                    generalStaff[numOfGeneralStaff] = staffMember1;
+	                    numOfGeneralStaff++;
+	                }
+	            }
+
+	            
+	            else if (personType.equals("Faculty:")) {
+	                String[] teacherString = scnr.nextLine().split(",");
+	                if (teacherString[0].equals(" true") || teacherString[0].equals(" false")) {
+	                	
+	                    if (teacherString[0].equals(" true")) {
+	                        Faculty teacher = new Faculty(true);
+	                        faculty[numOfFaculty] = teacher;
+	                        numOfFaculty++;
+	                    }
+	                }
+	              	                
+	                else if (teacherString.length > 1) {
+	                    if (teacherString[1].replace(" ", "").equals("true") || teacherString[1].replace(" ", "").equals("false")) {
+	                        String personDeptName = teacherString[0];
+	                        Boolean personIsTenured = false;
+	                        if (teacherString[1].equals(" true")) {
+	                            personIsTenured = true;
+	                        }
+	                        
+	                        Faculty teacher = new Faculty(personDeptName, personIsTenured);
+	                        faculty[numOfFaculty] = teacher;
+	                        numOfFaculty++;
+	                    }
+	                    else {
+	                        String personName = teacherString[0] ;
+	                        teacherString[1] = teacherString[1].replace(" ", "");
+	                        int personBirthYear = Integer.parseInt(teacherString[1]);
+	                        String personDeptName = teacherString[2].replace(" ", "");
+	                        Boolean personIsTenured = false;
+	                        
+	                        if (teacherString[3].replace(" ", "").equals("true")) {
+	                            personIsTenured = true;
+	                        }
+	                        
+	                        Faculty teacher = new Faculty(personName, personBirthYear, personDeptName, personIsTenured);
+	                        faculty[numOfFaculty] = teacher;
+	                        numOfFaculty++;
+	                    }
+	                }
+	           
+	                else {
 	                    Faculty teacher = new Faculty();
 	                    faculty[numOfFaculty] = teacher;
 	                    numOfFaculty++;
 	                }
-				}
-				
-				else if (array[0] == "GeneralStaff:") {
-					if (array.length == 1) {
-						GeneralStaff staffMember1 = new GeneralStaff();
-						generalStaff[numOfGeneralStaff] = staffMember1;
-						numOfGeneralStaff++;
-					}
-					if (array.length == 2) {
-						GeneralStaff staffMember1 = new GeneralStaff(array[1]);
-						generalStaff[numOfGeneralStaff] = staffMember1;
-						numOfGeneralStaff++;
-					}
-					if (array.length == 3) {
-						GeneralStaff staffMember1 = new GeneralStaff(array[1],array[2]);
-						generalStaff[numOfGeneralStaff] = staffMember1;
-						numOfGeneralStaff++;
-					}
-					if (array.length == 5) {
-						GeneralStaff staffMember1 = new GeneralStaff(array[1], Integer.parseInt(array[2]), array[3], array[4]);
-						generalStaff[numOfGeneralStaff] = staffMember1;
-						numOfGeneralStaff++;
-					}
-					else {
-						GeneralStaff staffMember1 = new GeneralStaff();
-						generalStaff[numOfGeneralStaff] = staffMember1;
-	                    numOfGeneralStaff++;
+	            }
+	            
+	            
+	            else if (personType.equals("Student:")) {
+	                String[] studentString = scnr.nextLine().split(",");
+	                if (studentString[0].equals("true")) {
+	                    Student student = new Student(true);
+	                    students[numOfStudents] = student;
+	                    numOfStudents++;
 	                }
-				}
-				
-				else if (array[0] == "Student:") {
-					if (array.length == 1) {
-						Student student = new Student();
-						students[numOfStudents] = student;
-						numOfStudents++;
-					}
-					if (array.length == 2) {
-						Student student = new Student(Boolean.parseBoolean(array[1]));
-						students[numOfStudents] = student;
-						numOfStudents++;
-					}
-					if (array.length == 3) {
-						Student student = new Student(array[1], Boolean.parseBoolean(array[2]));
-						students[numOfStudents] = student;
-						numOfStudents++;
-					}
-					if (array.length == 5) {
-						Student student = new Student(array[1], Integer.parseInt(array[2]), array[3], Boolean.parseBoolean(array[4]));
-						students[numOfStudents] = student;
-						numOfStudents++;
-					}
-					else {
-						Student student = new Student();
-						students[numOfStudents] = student;
-						numOfStudents++;
+	                else if (studentString[0].equals("false")) {
+	                    Student student = new Student(false);
+	                    students[numOfStudents] = student;
+	                    numOfStudents++;
 	                }
-				}
-				
-			}
-		} 
-		finally {
-			if (scnr != null) {
-				scnr.close();
-			}
-		}
+	                
+	                if (studentString.length > 1) {
+	                    if (studentString[1].replace(" ", "").equals("true")) {
+	                        String personMajor = studentString[0];
+	                        Student student = new Student(personMajor, true);
+	                        students[numOfStudents] = student;
+	                        numOfStudents++;
+	                    }
+	                    else if (studentString[1].replace(" ", "").equals("false")) {
+	                        String personMajor = studentString[0];
+	                        Student student = new Student(personMajor, false);
+	                        students[numOfStudents] = student;
+	                        numOfStudents++;
+	                    }
+	                    else {
+	                        String personName = studentString[0];
+	                        int personBirthYear = Integer.parseInt(studentString[1].replace(" ", ""));
+	                        String personMajor = studentString[2].replace(" ", "");
+	                        Boolean personIsGraduate = false;
+	                        
+	                        if (studentString[3].replace(" ", "").equals("true")) {
+	                            personIsGraduate = true;
+	                        }
+	                        Student student = new Student(personName, personBirthYear, personMajor, personIsGraduate);
+	                        students[numOfStudents] = student;
+	                        numOfStudents++;
+	                    }
+	                }
+	                
+	                else {
+	                    Student student = new Student();
+	                    students[numOfStudents] = student;
+	                    numOfStudents++;
+	                }
+	            }
+	        }		
+	        
+	        try (BufferedReader pleaseReadMyCode = new BufferedReader(new FileReader("SchoolDB_Initial.txt"))) {
+	            String lineOfMyCode;
+	            while ((lineOfMyCode = pleaseReadMyCode.readLine()) != null) {
+	                System.out.println(lineOfMyCode);
+	            }
+	        }
+	        
+	        System.out.println("\n**************************************************************\nSCHOOL DATABASE INFO:\n\n************************************************\nCOURSES:");
+
+	        if (numOfCourses > 0) {
+	            for (int i = 0; i < numOfCourses; i++) {
+	                System.out.println(courses[i].toString());
+	            }
+	            System.out.println("************************************************\n************************************************\nPERSONS:\n************************************************\n************************************************\nEMPLOYEES:\n************************************************\n************************************************\nGENERAL STAFF:");
+	        }
+	        if (numOfGeneralStaff > 0) {
+	            for (int j = 0; j < numOfGeneralStaff; j++) {
+	                System.out.println(generalStaff[j].toString());
+	            }
+	            System.out.println("************************************************\n************************************************\nFACULTY:");
+	        }
+	        if (numOfFaculty > 0) {
+	            for (int w = 0; w < numOfFaculty; w++) {
+	                System.out.println(faculty[w].toString());
+	            }
+	            System.out.println("************************************************\n************************************************\nSTUDENTS:");
+	        }
+	        if (numOfStudents > 0) {
+	            for (int l = 0; l < numOfStudents; l++) {
+	                System.out.println(students[l].toString());
+	            }
+	            System.out.println("************************************************\n**************************************************************\n");
+	        }
+	        fileByteStream.close();
+	    }
 	}
-}
