1409c114139c35520216d788765f7a9820504568
sarahdipietra <47428007+sarahdipietra@users.noreply.github.com>
2020-05-19 16:19:12 -0400

    Create InvalidDriverException.java

---
+++ Final Exam/InvalidDriverException.java
@@ -0,0 +1,12 @@
+package FinalExam;
+
+public class InvalidDriverException extends Exception {
+	public InvalidDriverException() {
+		
+	}
+	
+	public InvalidDriverException(String message) {
+		super(message);
+	}
+
+}
