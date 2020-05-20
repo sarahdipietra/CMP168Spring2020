799033c71cd2136d6e0a6cb22e844b0d6137d942
sarahdipietra <47428007+sarahdipietra@users.noreply.github.com>
2020-05-20 19:56:04 -0400

    Add files via upload

---
+++ Final Exam/Announcements.java
@@ -0,0 +1,33 @@
+package FinalExam;
+
+public interface Announcements {
+	
+	public String departure();
+	public String arrival();
+	public String doNotDisturbTheDriver();
+	
+	/*String departure = "All Aboard\n";
+	super.departure("The Bus\n");
+	String arrival = "Everyone Out\n";
+	super.departure("Of The Bus\n");
+
+	
+	public String departure() {
+		System.out.print("All Aboard\n");
+	}
+	
+	public String departure() implements Bus {
+		super.departure();
+		System.out.print(super.departure() + "The Bus\n");
+	}
+	
+	public String arrival() {
+		System.out.print("All Aboard\n");
+	}
+	
+	public String arrival() implements Bus {
+		super.arrival();
+		System.out.print(super.arrival() + "The Bus\n");
+	}8
+	*/
+}
